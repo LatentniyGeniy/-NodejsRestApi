9059831c8fc8716f4cb5faf368d8f1e9fe93ce89
LatentniyGeniy <vizzza99@gmail.com>
2021-11-13 12:33:10 +0300

    feat: create services

--- src/resources/abiturient/abiturient.service.js
+++ src/resources/abiturient/abiturient.service.js
@@ -1,5 +1,24 @@
-const usersRepo = require('./aditurient.memory.repository');
+const AbiturientsRepo = require('./abiturient.memory.repository');
+const ExamRepo = require('../exam/exam.memory.repository')
 
-const getAll = () => usersRepo.getAll();
+const getAll = () => AbiturientsRepo.getAll();
 
-module.exports = { getAll };
+const getById = (id) => AbiturientsRepo.getById(id);
+
+const getExamsByAbiturientId = (abiturientID) => ExamRepo.getExamsByAbiturientId(abiturientID);
+
+const createAbiturient = ({ lastName, firstName, numCertificate }) =>
+  AbiturientsRepo.createAbiturient({ lastName, firstName, numCertificate });
+
+const deleteById = async (id) => {
+  const abiturientDeletable = await getById(id);
+  AbiturientsRepo.deleteById(id);
+  ExamRepo.deleteAbiturientFromExam(id)
+
+  return abiturientDeletable;
+};
+
+const updateById = ({ id, lastName, firstName, numCertificate }) =>
+  AbiturientsRepo.updateById({ id, lastName, firstName, numCertificate });
+
+module.exports = { getAll, getById, createAbiturient, deleteById, updateById, getExamsByAbiturientId };
