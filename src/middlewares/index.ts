fa4043a8949bcdd4cc77e0277bded9e4a8fbbf65
LatentniyGeniy <vizzza99@gmail.com>
2021-11-24 13:41:22 +0300

    feat: create errorHandler

---
+++ src/middlewares/index.ts
@@ -0,0 +1,4 @@
+import { errorHandler } from './errorHandler';
+import { successHttpLogger, errorHttpLogger } from './morganMiddleware';
+
+export {  successHttpLogger, errorHttpLogger, errorHandler };
