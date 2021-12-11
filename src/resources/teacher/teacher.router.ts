bc06e4bd107a89a0fcf8af1776c4d7de729600e1
LatentniyGeniy <vizzza99@gmail.com>
2021-12-11 16:07:49 +0300

    feat: add typeorm for teachers

--- src/resources/teacher/teacher.router.ts
+++ src/resources/teacher/teacher.router.ts
@@ -1,26 +1,24 @@
 import { StatusCodes } from 'http-status-codes';
 import { Request, Response, Router } from 'express';
+import asyncHandler from 'express-async-handler';
 
-import Teacher from './teacher.model';
-import Exam from '../exam/exam.model';
+import Teacher from './teacher.entity';
 import TeacherService from './teacher.service';
-import catchErrors from '../../common/catchErrors';
+import Exam from '../exam/exam.entity';
 
 const router = Router({ mergeParams: true });
 
 router.route('/').get(
-  catchErrors(async (_req: Request, res: Response) => {
+  asyncHandler(async (_req: Request, res: Response) => {
     const teachers = await TeacherService.getAll();
-
-    res.json(teachers.map(Teacher.toResponse));
+    res.status(StatusCodes.OK).json(teachers.map(Teacher.toResponse));
   })
 );
 
 router.route('/').post(
-  catchErrors(async (req: Request, res: Response) => {
-    const { lastName, firstName, degree } = req.body;
+  asyncHandler(async (req: Request, res: Response) => {
 
-    const teacher = await TeacherService.createTeacher({ lastName, firstName, degree });
+    const teacher = await TeacherService.createTeacher(req.body);
 
     if (teacher) {
       res.status(StatusCodes.CREATED).json(Teacher.toResponse(teacher));
@@ -33,10 +31,10 @@ router.route('/').post(
 );
 
 router.route('/:id').get(
-  catchErrors(async (req: Request, res: Response) => {
+  asyncHandler(async (req: Request, res: Response) => {
     const { id } = req.params;
 
-    const teacher = await TeacherService.getById(id || '');
+    const teacher = await TeacherService.getById(id!);
 
     if (teacher) {
       res.json(Teacher.toResponse(teacher));
@@ -49,11 +47,10 @@ router.route('/:id').get(
 );
 
 router.route('/:id').put(
-  catchErrors(async (req: Request, res: Response) => {
+  asyncHandler(async (req: Request, res: Response) => {
     const { id } = req.params;
-    const { lastName, firstName, degree } = req.body;
 
-    const teacher = await TeacherService.updateById({ id: id || '', lastName, firstName, degree });
+    const teacher = await TeacherService.updateById(id!, req.body);
 
     if (teacher) {
       res.status(StatusCodes.OK).json(Teacher.toResponse(teacher));
@@ -66,28 +63,27 @@ router.route('/:id').put(
 );
 
 router.route('/:id').delete(
-  catchErrors(async (req: Request, res: Response) => {
+  asyncHandler(async (req: Request, res: Response) => {
     const { id } = req.params;
 
-    const teacher = await TeacherService.deleteById(id || '');
+    const teacher = await TeacherService.deleteById(id!);
 
     if (!teacher) {
-      return res
+      res
         .status(StatusCodes.NOT_FOUND)
         .json({ code: 'TEACHER_NOT_FOUND', msg: 'Teacher not found' });
     }
-
-    return res
+    res
       .status(StatusCodes.NO_CONTENT)
       .json({ code: 'TEACHER_DELETED', msg: 'The teacher has been deleted' });
   })
 );
 
 router.route('/:teacherId/exams').get(
-  catchErrors(async (req: Request, res: Response) => {
+  asyncHandler(async (req: Request, res: Response) => {
     const  {teacherId}  = req.params;
 
-    const exam = await TeacherService.getExamsByTeacherId(teacherId || '');
+    const exam = await TeacherService.getExamsByTeacherId(teacherId!);
 
     if (exam) {
       res.json(exam.map(Exam.toResponse));
--- src/resources/teacher/teacher.service.ts
+++ src/resources/teacher/teacher.service.ts
@@ -1,26 +1,56 @@
-import TeachersRepo from './teacher.memory.repository';
-import ExamRepo from '../exam/exam.memory.repository';
-import {TTeacherModel, TTeacher} from './teacher.type'
-import { TExamModel } from '../exam/exam.type';
+import { getCustomRepository } from 'typeorm';
+import { TeacherRepository } from './teacher.repository';
+import Teacher from './teacher.entity'
 
-const getAll = async (): Promise<TTeacherModel[]> => TeachersRepo.getAll();
+import { ExamRepository } from '../exam/exam.repository';
+import Exam from '../exam/exam.entity';
 
-const getById = async (id: string): Promise<TTeacherModel | null> => TeachersRepo.getById(id);
+const getAll = async (): Promise<Teacher[]> => {
+  const teacherRepository = getCustomRepository(TeacherRepository);
+  return teacherRepository.getAllTeachers();
+};
 
-const getExamsByTeacherId = async (studentId: string): Promise<TExamModel[]> => ExamRepo.getExamsByTeacherId(studentId);
+const getById = async (id: string): Promise<Teacher | null> => {
+  const teacherRepository = getCustomRepository(TeacherRepository);
+  const teacher = await teacherRepository.getById(id);
+  if (!teacher) return null;
+  return teacher;
+};
 
-const createTeacher = async ({ lastName, firstName, degree }: TTeacher): Promise<TTeacherModel> =>
-  TeachersRepo.createTeacher({ lastName, firstName, degree });
+const createTeacher = async (data: Omit<Teacher, 'id'>): Promise<Teacher> => {
+  const teacherRepository = getCustomRepository(TeacherRepository);
+  const teacher = await teacherRepository.createTeacher(data);
+  return teacher;
+};
 
-const deleteById = async (id:string): Promise<TTeacherModel | null> => {
-  const teacherDeletable = await getById(id);
-  await TeachersRepo.deleteById(id);
-  await ExamRepo.deleteTeacherFromExam(id);
+const deleteById = async (id: string): Promise<Teacher | null> => {
+  const teacherRepository = getCustomRepository(TeacherRepository);
+  const teacherDeletable = await teacherRepository.getById(id);
+  const examRepository = getCustomRepository(ExamRepository)
+
+  if (!teacherDeletable) return null;
+
+  const exams = await examRepository.getExamsByTeacherId(id);
+  exams.map(async ex=>ex.studentId?examRepository.updateByTEID(id):examRepository.deleteById(ex.id))
+  await teacherRepository.deleteById(id);
 
   return teacherDeletable;
 };
 
-const updateById = async (teacher: TTeacherModel): Promise<TTeacherModel | null> =>
-  TeachersRepo.updateById(teacher);
+const updateById = async (id: string, data: Omit<Teacher, 'id'>): Promise<Teacher | null> => {
+  const teacherRepository = getCustomRepository(TeacherRepository);
+  await teacherRepository.updateById(id, data);
+  const teacher = await teacherRepository.getById(id);
+  if (!teacher) return null;
+  return teacher;
+};
 
-export default { getAll, getById, createTeacher, deleteById, updateById,getExamsByTeacherId };
+const getExamsByTeacherId = async (teacherId: string): Promise<Exam[] | null> => {
+  const examRepository = getCustomRepository(ExamRepository);
+  const exams = await examRepository.getExamsByTeacherId(teacherId!)
+  if (!exams) return null;
+  return exams
+}
+
+export default { getAll, getById, createTeacher, deleteById, updateById, getExamsByTeacherId };
+
